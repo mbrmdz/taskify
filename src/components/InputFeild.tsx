e81d2a9b53604a5018bf59057861c3cf56c972c3
Michelle Bermudez <michelleg.bermudez@gmail.com>
2024-04-13 00:00:44 +0800

    Initial commit

---
+++ src/components/InputFeild.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputFeild = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" />
+    </form>
+  );
+};
+
+export default InputFeild;
